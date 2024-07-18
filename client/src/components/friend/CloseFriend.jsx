0daf70fce2f60a8c6c807728c53c067779dad149
ngoguessfidele <ngoguessfidele@gmail.com>
2024-07-18 23:27:33 +0300

    all tasks

---
+++ client/src/components/friend/CloseFriend.jsx
@@ -0,0 +1,14 @@
+import './closeFriend.css'
+
+const CloseFriend = ({user}) => {
+  return (
+    <div>
+      <li className="sidebarFriend">
+            <img src={user.profilePicture} alt='' className='sidebarFriendImg' />
+            <span className='sidebarFriendName'>{user.username}</span>
+          </li>
+    </div>
+  )
+}
+
+export default CloseFriend
